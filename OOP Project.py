fcd4a8171e79dc0139303e69b8a5d16597bd7c90
Jhern35 <jonathan.hernandez13@utrgv.edu>
2025-04-02 01:58:22 +0000

    Added abstract methods

--- OOP Project.py
+++ OOP Project.py
@@ -1,4 +1,21 @@
 import random
+from abc import ABC, abstractmethod
+
+
+# We will modify as needed. 
+class Game(ABC):
+    @abstractmethod
+    def game_over():
+        pass
+
+    @abstractmethod
+    def rules():
+        pass
+
+    @abstractmethod
+    def player_cards():
+        pass
+
 
 class Card:
     def __init__(self, number, suit, color):
@@ -10,29 +27,7 @@ class Card:
 
 class Deck(Card):
     def __init__(self):
-        self.num_cards = []
-    
-    def sym(self, num):
-        if (self.num < 11 and self.num > 1):
-            return str(self.num)
-        elif (self.num == 1):
-            return "Ace"
-        elif (self.num == 11):
-            return "Jack"
-        elif (self.num == 12):
-            return "Queen"
-        else:
-            return "King"
-        
-    def rank(self):
-        return random(1, 13)
-        
-    def deck(self):
-        for i in range(0, self.num_cards):
-            rank = rank()
-            symbol = sym(rank)
-            shade = col()
-            super().__init__(rank, sym, col)
+        pass
     
    
     
